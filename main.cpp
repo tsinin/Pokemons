406746c1ed1d3902eedeb1024600b43eeb02a440
tsinin <andrew.tsinin@mail.ru>
2019-05-10 18:13:34 +0300

    option Test

--- main.cpp
+++ main.cpp
@@ -1,13 +1,13 @@
-#include <gtest/gtest.h>
-#include <gmock/gmock.h>
+/*#include <gtest/gtest.h>
+#include <gmock/gmock.h>*/
 #include <iostream>
 #include "PokemonsGame/Game.h"
 
 
-int main(int argc, char* argv[]) {
-    /*Game* game = new Game();
+int main() {
+    Game* game = new Game();
     game->run();
-    delete game;*/
-    testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+    delete game;
+    /*testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();*/
 }
---
+++ mainForTest.cpp
@@ -0,0 +1,8 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include <iostream>
+
+int main() {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
